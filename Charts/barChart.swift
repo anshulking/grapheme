d8f0400130e2b6fa94b4ba1475ce6e49e5fdd327
MAC238 <MAC238@Anshul-MAC238.local>
2017-08-14 18:21:39 +0530

    first commit

---
+++ Charts/barChart.swift
@@ -0,0 +1,9 @@
+//
+//  barChart.swift
+//  Charts
+//
+//  Created by MAC238 on 8/14/17.
+//  Copyright © 2017 tatvasoft. All rights reserved.
+//
+
+import Foundation
---
+++ PieChartView.swift
@@ -0,0 +1,21 @@
+//
+//  PieChartView.swift
+//  Charts
+//
+//  Created by MAC238 on 8/11/17.
+//  Copyright © 2017 tatvasoft. All rights reserved.
+//
+
+import UIKit
+
+class PieChartView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
